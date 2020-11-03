1ff1809cc8f9dd3849b7bcc92819c8774751f125
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-11-03 08:56:31 -0800

    update  bootstrap data

--- beer-service/src/main/java/com/vignesh/beerservice/bootstrap/BeerLoader.java
+++ beer-service/src/main/java/com/vignesh/beerservice/bootstrap/BeerLoader.java
@@ -13,13 +13,16 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 @Component
 public class BeerLoader implements CommandLineRunner {
-    @Autowired
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
+
     private final BeerRepository beerRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
-
+        if(beerRepository.count() == 0 ) {
             loadBeerObjects();
         }
     }
@@ -31,7 +34,7 @@ public class BeerLoader implements CommandLineRunner {
                 .minOnHand(12)
                 .quantityToBrew(200)
                 .price(new BigDecimal("12.95"))
-
+                .upc(BEER_1_UPC)
                 .build();
 
         Beer b2 = Beer.builder()
@@ -40,6 +43,7 @@ public class BeerLoader implements CommandLineRunner {
                 .minOnHand(12)
                 .quantityToBrew(200)
                 .price(new BigDecimal("12.95"))
+                .upc(BEER_2_UPC)
                 .build();
 
         Beer b3 = Beer.builder()
@@ -48,14 +52,11 @@ public class BeerLoader implements CommandLineRunner {
                 .minOnHand(12)
                 .quantityToBrew(200)
                 .price(new BigDecimal("12.95"))
-
+                .upc(BEER_3_UPC)
                 .build();
 
         beerRepository.save(b1);
         beerRepository.save(b2);
         beerRepository.save(b3);
     }
-}
-
-
 }
