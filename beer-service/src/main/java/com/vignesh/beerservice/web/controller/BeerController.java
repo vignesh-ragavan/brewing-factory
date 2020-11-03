486228d06e4a892314c3652f482e2521ca82bb04
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-11-03 08:36:44 -0800

    added controller

---
+++ beer-service/src/main/java/com/vignesh/beerservice/web/controller/BeerController.java
@@ -0,0 +1,38 @@
+package com.vignesh.beerservice.web.controller;
+
+
+import com.vignesh.beerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+
+
+@RequestMapping("/api/v1/")
+@RestController
+public class BeerController {
+
+
+
+    @GetMapping("beer/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+        return new ResponseEntity<>(BeerDto.builder().build(),HttpStatus.OK );
+        }
+
+
+@PostMapping(path = "beer")
+public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        return new ResponseEntity<>( HttpStatus.CREATED);
+        }
+
+@PutMapping("beer/{beerId}")
+public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        return new ResponseEntity<>( HttpStatus.NO_CONTENT);
+        }
+
+        }
---
+++ beer-service/src/main/java/com/vignesh/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,4 @@
+package com.vignesh.beerservice.web.model;
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
